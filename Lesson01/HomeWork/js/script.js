6d4ce6a6f721ae63b7e9ffb5ee61efc4bd618db1
sonshiko <oleksandr.grebin@gmail.com>
2023-12-30 12:36:04 +0200

    updated git.txt in LESSON01

--- Lesson01/HomeWork/js/script.js
+++ Lesson01/HomeWork/js/script.js
@@ -1,48 +1,55 @@
 //***1***
 // Робота зі змінними
 // Оголосіть дві змінні: name та city.
-let name1, city;
+// let username, city;
 
-// Присвойте значення "Іван" змінній name.
-name1 = 'Іван';
+// // Присвойте значення "Іван" змінній name.
+// username = 'Іван';
 
-// Скопіюйте значення зі змінної name в city.
-city = name1;
+// // Скопіюйте значення зі змінної name в city.
+// city = username;
 
-// Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
-console.log(city);
+// // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
+// console.log(city);
 
 //***2***
 //Який буде результат виконання скрипта?
-let name = 'Olga';
-console.log(`привіт ${1}`); //           привіт 1
-console.log(`привіт ${'name'}`); //      привіт name
-console.log(`привіт ${name}`); // ?      привіт Olga
+// let name = 'Olga';
+// console.log(`привіт ${1}`); //           привіт 1
+// console.log(`привіт ${'name'}`); //      привіт name
+// console.log(`привіт ${name}`); // ?      привіт Olga
 
 //***3***
 //Видобути число зі змінних
-// let a = "5";
-console.log(typeof a);
-// let b = "13cvb";
-// let c = "12.9sxdcfgv";
+// let a = '5';
+//console.log(typeof a);
+// let b = '13cvb';
+// let c = '12.9sxdcfgv';
 // вивести в консоль тип
+// a = parseInt(a);
+// console.log(typeof a, 'a=', a);
+// b = parseInt(b);
+// console.log(typeof b, 'b=', b);
+// c = parseFloat(c);
+// console.log(typeof c, 'c=', c);
 
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
 
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
-
+// console.log(Math.max(20, 10, 50, 40));
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
-
+// console.log(Math.random() * (4 - 2) + 2);
 //***7**
 //дізнатись довжину message
-// const message = "Welcome to Bahamas!";
-
+// const message = 'Welcome to Bahamas!';
+// console.log(message.length);
 //***8**
 //вивести в консоль message  великими літерами
-
+// console.log(message.toUpperCase());
 //***9**
 // створити пустий об*єкт
 // додати туди ім*я, вік і місто
@@ -50,6 +57,17 @@ console.log(typeof a);
 // видалити місто
 // додати буль з ключем: like flowers
 // вивести результат в консоль
-
+// let obj = {};
+// obj.name = 'Olena';
+// obj.age = 44;
+// obj.city = 'Bagacheve';
+// obj['like flowers'] = true;
+// console.log(obj);
+// delete obj.city;
+// console.log(obj);
 //***10**
 // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+// for (key in obj) {
+//   console.log(key);
+//   console.log(obj[key]);
+// }
